58b6c601b3f917358002ca6aa10f6fbb584bc855
Raphaël Vinot <raphael@vinot.info>
2020-09-22 18:35:10 +0200

    chg: Change local-site.yml to build HEAD

--- docs_build.sh
+++ docs_build.sh
@@ -2,11 +2,11 @@
 cd "${0%/*}" # cd to the folder with script
 antora #Verify Antora is installed
 RESULT=($?)
-if [ $RESULT -eq 127 ]; then #if Antora is not found in $PATH (probably not installed) 
+if [ $RESULT -eq 127 ]; then #if Antora is not found in $PATH (probably not installed)
     echo -e '>> \e[31mFAIL\e[0m Antora not found in \$PATH, Install Antora from https://docs.antora.org/antora/2.2/install/install-antora/'
     exit 1
 elif [ -f local-site.yml ]; then
-    rm -rf build/ docs/ #remove the existing docs. Just in case there are docs that should not be there. 
+    rm -rf build/ docs/ 404.html index.html sitemap.xml robots.txt #remove the existing docs. Just in case there are docs that should not be there.
     echo -e '>> \e[34mINFO\e[0m Building docs with Antora'
     antora --fetch local-site.yml # build documentation with antora
     echo -e '>> \e[34mINFO\e[0m Copying generated docs to the root folder.'
